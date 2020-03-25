5935fd53939250888e6c60ece2180256a3dedefd
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2020-03-25 15:50:57 -0300

    Update with pylint.

--- chb_label_wrapper.py
+++ chb_label_wrapper.py
@@ -1,11 +1,12 @@
-from abc import abstractmethod, ABCMeta
 from collections import OrderedDict
-from re import compile
+from re import compile as compile_re
+
 
 class ChbLabelWrapper:
     """
     Class for handling the labels
     """
+
     def __init__(self, filename):
         self._filename = filename
         self._file = open(filename, 'r')
@@ -29,7 +30,7 @@ class ChbLabelWrapper:
 
         # Block two-N
         self._metadata_store = self._parse_file_metadata(blocks[2:])
-        
+
         file_obj.close()
 
     def _parse_frequency(self, frequency_block):
@@ -39,11 +40,12 @@ class ChbLabelWrapper:
         :param frequency_block: Frequency block
         :return: Parses and returns the frequency value in Hz
         """
-        pattern = compile("Data Sampling Rate: (.*?) Hz")
+        pattern = compile_re("Data Sampling Rate: (.*?) Hz")
         result = pattern.search(frequency_block)
         # Check if there is a match or not
         if result is None:
-            raise ValueError('Frequency block does not contain the correct string ("Data Sampling Rate: __ Hz")')
+            raise ValueError(
+                'Frequency block does not contain the correct string ("Data Sampling Rate: __ Hz")')
         result = int(result.group(1))
         return result
 
@@ -55,7 +57,7 @@ class ChbLabelWrapper:
         """
         # Split by line
         lines = channel_block.split('\n')
-        pattern = compile("Channel [0-9]{1,}: (.*?)$")
+        pattern = compile_re("Channel [0-9]{1,}: (.*?)$")
 
         output_channel_list = []
         for line in lines:
@@ -74,25 +76,32 @@ class ChbLabelWrapper:
         :return:
         """
         # Search first line for seizure file pattern
-        pattern_filename = compile("File Name: (.*?)$")
-        pattern_start_time = compile("File Start Time: (.*?)$")
-        pattern_end_time = compile("File End Time: (.*?)$")
-        pattern_seizures = compile("Number of Seizures in File: (.*?)$")
-        pattern_seizure_start = compile("Seizure [0-9]{0,}[ ]{0,}Start Time: (.*?) seconds")
-        pattern_seizure_end = compile("Seizure [0-9]{0,}[ ]{0,}End Time: (.*?) seconds")
+        pattern_filename = compile_re("File Name: (.*?)$")
+        pattern_start_time = compile_re("File Start Time: (.*?)$")
+        pattern_end_time = compile_re("File End Time: (.*?)$")
+        pattern_seizures = compile_re("Number of Seizures in File: (.*?)$")
+        pattern_seizure_start = compile_re(
+            "Seizure [0-9]{0,}[ ]{0,}Start Time: (.*?) seconds")
+        pattern_seizure_end = compile_re(
+            "Seizure [0-9]{0,}[ ]{0,}End Time: (.*?) seconds")
 
         if pattern_filename.search(metadata_block[0]) is not None:
             file_metadata = dict()
             filename = pattern_filename.search(metadata_block[0]).group(1)
-            file_metadata['start_time'] = pattern_start_time.search(metadata_block[1]).group(1)
-            file_metadata['end_time'] = pattern_end_time.search(metadata_block[2]).group(1)
-            file_metadata['n_seizures'] = int(pattern_seizures.search(metadata_block[3]).group(1))
+            file_metadata['start_time'] = pattern_start_time.search(
+                metadata_block[1]).group(1)
+            file_metadata['end_time'] = pattern_end_time.search(
+                metadata_block[2]).group(1)
+            file_metadata['n_seizures'] = int(
+                pattern_seizures.search(metadata_block[3]).group(1))
             file_metadata['channel_names'] = self._channel_names
             file_metadata['sampling_rate'] = self.get_sampling_rate()
             seizure_intervals = []
             for i in range(file_metadata['n_seizures']):
-                seizure_start = int(pattern_seizure_start.search(metadata_block[4 + i * 2]).group(1))
-                seizure_end = int(pattern_seizure_end.search(metadata_block[4 + i * 2 + 1]).group(1))
+                seizure_start = int(pattern_seizure_start.search(
+                    metadata_block[4 + i * 2]).group(1))
+                seizure_end = int(pattern_seizure_end.search(
+                    metadata_block[4 + i * 2 + 1]).group(1))
                 seizure_intervals.append((seizure_start, seizure_end))
             file_metadata['seizure_intervals'] = seizure_intervals
             output_metadata[filename] = file_metadata
@@ -101,17 +110,19 @@ class ChbLabelWrapper:
             #warnings.warn("Block didn't follow the pattern for a metadata file block", Warning)
             # Check channel names
             try:
-                self._channel_names = self._parse_channel_names("\n".join(metadata_block))
-            except Exception as e:
-                print ('Failed to parse block as a channel names block')
-                raise e
+                self._channel_names = self._parse_channel_names(
+                    "\n".join(metadata_block))
+            except Exception as error:
+                print('Failed to parse block as a channel names block')
+                raise error
         return output_metadata
 
     def _parse_file_metadata(self, seizure_file_blocks):
         """
         Parse the file metadata list blocks to get the seizure intervals
 
-        Note: These are not necessarily in file order, so always check against the filename before continuing.
+        Note: These are not necessarily in file order, so always
+        check against the filename before continuing.
         :param seizure_file_blocks: List of seizure file blocks
         """
         output_metadata = OrderedDict()
@@ -137,7 +148,8 @@ class ChbLabelWrapper:
         """
         Get list of seizure intervals for each file
         """
-        return [metadata['seizure_intervals'] for filename, metadata in self._metadata_store.items()]
+        return [metadata['seizure_intervals']
+                for filename, metadata in self._metadata_store.items()]
 
     def get_file_metadata(self):
         """
